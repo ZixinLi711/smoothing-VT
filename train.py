13ce3af54e86577b5390e7bd9bbd7f3419207f44
ZixinLi711 <74847775+ZixinLi711@users.noreply.github.com>
2021-07-01 21:37:27 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,339 @@
+# this file is based on code publicly available at
+#   https://github.com/bearpaw/pytorch-classification
+# written by Wei Yang.
+
+import argparse
+import os
+import torch
+import PIL
+import torchvision
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+from datasets import get_dataset, DATASETS
+from architectures import ARCHITECTURES, get_architecture
+from torch.optim import SGD, Optimizer
+from torch.optim.lr_scheduler import StepLR
+import time
+import datetime
+import torch.nn.functional as F
+from train_utils import AverageMeter, accuracy, init_logfile, log
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('dataset', type=str, choices=DATASETS)
+parser.add_argument('arch', type=str, choices=ARCHITECTURES)
+parser.add_argument('outdir', type=str, help='folder to save model and training log)')
+parser.add_argument('--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--batch', default=256, type=int, metavar='N',
+                    help='batchsize (default: 256)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    help='initial learning rate', dest='lr')
+parser.add_argument('--lr_step_size', type=int, default=30,
+                    help='How often to decrease learning by gamma.')
+parser.add_argument('--gamma', type=float, default=0.1,
+                    help='LR is multiplied by gamma on schedule.')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--noise_sd', default=0.0, type=float,
+                    help="standard deviation of Gaussian noise for data augmentation")
+parser.add_argument('--gpu', default=None, type=str,
+                    help='id(s) for CUDA_VISIBLE_DEVICES')
+parser.add_argument('--print-freq', default=100, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+args = parser.parse_args()
+
+
+def main():
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    if not os.path.exists(args.outdir):
+        os.mkdir(args.outdir)
+
+    print(args.noise_sd)
+    train_dataset = get_dataset(args.dataset, 'train')
+    test_dataset = get_dataset(args.dataset, 'test')
+    pin_memory = (args.dataset == "imagenet")
+    train_loader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch,
+                              num_workers=args.workers, pin_memory=pin_memory)
+    test_loader = DataLoader(test_dataset, shuffle=False, batch_size=args.batch,
+                             num_workers=args.workers, pin_memory=pin_memory)
+
+    model = get_architecture(args.arch, args.dataset)
+    
+
+    logfilename = os.path.join(args.outdir, 'log.txt')
+    init_logfile(logfilename, "epoch\ttime\tlr\ttrain loss\ttrain acc\ttestloss\ttest acc")
+
+    criterion = CrossEntropyLoss().cuda()
+    optimizer = SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    scheduler = StepLR(optimizer, step_size=args.lr_step_size, gamma=args.gamma)
+
+    for epoch in range(args.epochs):
+        scheduler.step(epoch)
+        before = time.time()
+        if args.arch == "VT":
+            BATCH_SIZE_TRAIN = 100
+            BATCH_SIZE_TEST = 100
+            DL_PATH = "C:\Pytorch\Spyder\CIFAR10_data"
+            transform = torchvision.transforms.Compose([torchvision.transforms.RandomHorizontalFlip(),
+     torchvision.transforms.RandomRotation(10, resample=PIL.Image.BILINEAR),
+     torchvision.transforms.RandomAffine(8, translate=(.15, .15)),
+     torchvision.transforms.ToTensor(),
+     torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+            train_dataset = torchvision.datasets.CIFAR10(DL_PATH, train=True,
+                                             download=True, transform=transform)
+
+            test_dataset = torchvision.datasets.CIFAR10(DL_PATH, train=False,
+                                            download=True, transform=transform)
+
+            train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE_TRAIN,
+                                           shuffle=True)
+            test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE_TEST,
+                                          shuffle=False)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+            train_loss, train_acc = trainVT(model, optimizer, train_loader, epoch, args.noise_sd)
+            test_loss, test_acc = testVT(model, test_loader, args.noise_sd)
+        else:
+            train_loss, train_acc = train(train_loader, model, criterion, optimizer, epoch, args.noise_sd)
+            test_loss, test_acc = test(test_loader, model, criterion, args.noise_sd)
+        after = time.time()
+
+        log(logfilename, "{}\t{:.3}\t{:.3}\t{:.3}\t{:.3}\t{:.3}\t{:.3}".format(
+            epoch, str(datetime.timedelta(seconds=(after - before))),
+            scheduler.get_lr()[0], train_loss, train_acc, test_loss, test_acc))
+
+        torch.save({
+            'epoch': epoch + 1,
+            'arch': args.arch,
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }, os.path.join(args.outdir, 'checkpoint.pth.tar'))
+
+
+def train(loader: DataLoader, model: torch.nn.Module, criterion, optimizer: Optimizer, epoch: int, noise_sd: float):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    end = time.time()
+
+    # switch to train mode
+    model.train()
+
+    for i, (inputs, targets) in enumerate(loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        inputs = inputs.cuda()
+        targets = targets.cuda()
+
+        # augment inputs with noise
+        inputs = inputs + torch.randn_like(inputs, device='cuda') * noise_sd
+
+        # compute output
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(outputs, targets, topk=(1, 5))
+        losses.update(loss.item(), inputs.size(0))
+        top1.update(acc1.item(), inputs.size(0))
+        top5.update(acc5.item(), inputs.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                epoch, i, len(loader), batch_time=batch_time,
+                data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+    return (losses.avg, top1.avg)
+
+def trainVT(model, optimizer, data_loader, epoch, noise_sd):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    end = time.time()
+
+    # switch to train mode
+    model.train()
+
+    for i, (data, target) in enumerate(data_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        data = data.cuda()
+        target = target.cuda()
+
+        #print(model)
+
+        # augment inputs with noise
+        data = data + torch.randn_like(data, device='cuda') * noise_sd
+
+        optimizer.zero_grad()
+        output = F.log_softmax(model(data), dim=1)
+        loss = F.nll_loss(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), data.size(0))
+        top1.update(acc1.item(), data.size(0))
+        top5.update(acc5.item(), data.size(0))
+
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                epoch, i, len(data_loader), batch_time=batch_time,
+                data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+    return (losses.avg, top1.avg)
+
+def testVT(model, data_loader, noise_sd):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    end = time.time()
+
+    model.eval()
+
+    total_samples = len(data_loader.dataset)
+    correct_samples = 0
+    total_loss = 0
+
+    with torch.no_grad():
+        for i, (data, target)  in enumerate(data_loader):
+            # measure data loading time
+            data_time.update(time.time() - end)
+
+            data = data.cuda()
+            target = target.cuda()
+
+
+            # augment inputs with noise
+            data = data + torch.randn_like(data, device='cuda') * noise_sd
+
+            output = F.log_softmax(model(data), dim=1)
+            loss = F.nll_loss(output, target, reduction='sum')
+            _, pred = torch.max(output, dim=1)
+
+            total_loss += loss.item()
+            correct_samples += pred.eq(target).sum()
+
+            avg_loss = total_loss / total_samples
+            #loss_history.append(avg_loss)
+            '''
+            print('\nAverage test loss: ' + '{:.4f}'.format(avg_loss) +
+              '  Accuracy:' + '{:5}'.format(correct_samples) + '/' +
+              '{:5}'.format(total_samples) + ' (' +
+              '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
+            '''
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), data.size(0))
+            top1.update(acc1.item(), data.size(0))
+            top5.update(acc5.item(), data.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                    i, len(data_loader), batch_time=batch_time,
+                    data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+    return (losses.avg, top1.avg)
+
+
+
+def test(loader: DataLoader, model: torch.nn.Module, criterion, noise_sd: float):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    end = time.time()
+
+    # switch to eval mode
+    model.eval()
+
+    with torch.no_grad():
+        for i, (inputs, targets) in enumerate(loader):
+            # measure data loading time
+            data_time.update(time.time() - end)
+
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+            # augment inputs with noise
+            inputs = inputs + torch.randn_like(inputs, device='cuda') * noise_sd
+
+            # compute output
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(outputs, targets, topk=(1, 5))
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(acc1.item(), inputs.size(0))
+            top5.update(acc5.item(), inputs.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                    i, len(loader), batch_time=batch_time,
+                    data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+        return (losses.avg, top1.avg)
+
+
+
+if __name__ == "__main__":
+    main()
